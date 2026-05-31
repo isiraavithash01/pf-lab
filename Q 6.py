70737932dbf99a40e1e469f0d3eaf9725d563e95
isiraavithash01 <isiraavithash01@gmail.com>
2026-06-01 02:13:07 +0530

    Question 6: Gym Membership Payment System

--- Q 6.py
+++ Q 6.py
@@ -1,3 +1,29 @@
-#hi
+#Gym Membership Payment System 
 
-print("hello world")
+#Inputs
+
+monthly_fee=float(input("Enter the monthly fee: "))
+number_of_months=int(input("Enter the number of months: "))
+registration_fee=float(input("Enter the registration fee: "))
+personal_trainer_fee=float(input("Enter the personal trainer fee: "))
+
+
+
+#Calculations
+
+total=(monthly_fee*number_of_months)+registration_fee+personal_trainer_fee
+
+tax=total*5/100
+
+final_payment=total+tax
+
+
+#Output
+
+print("--- Gym Membership Invoice ---")
+
+print(f"Total: $",total)
+
+print(f"Tax : $",tax)
+print(f"Final payment: $",final_payment)
+print(f"Thank you!")
